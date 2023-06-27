4d3dc82256aaf121385822abe0f834dba8ce6fb0
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-26 22:21:03 -0300

    configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable}`}>
+        <main className='flex items-center justify-center w-screen h-screen px-8 bg-black'>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
